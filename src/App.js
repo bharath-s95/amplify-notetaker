2dfb40ad275a56d2ed715dc73cf25323affa8b42
bharath-s95 <36877669+bharath-s95@users.noreply.github.com>
2020-03-28 14:30:47 +0530

    publish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { withAuthenticator } from "aws-amplify-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    notes: [{
+      id: 1,
+      note: 'Hello World!'
+    }]
+  }
+  renderNotes = (notes) => {
+    return notes.map(note => {
+      return (
+        <div className="flex items-center">
+          {note.note}
+          <button>&times;</button>
+        </div>
+      )
+    })
+  }
+  render() {
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red" style={{ textAlign: 'center', backgroundColor: '#fce2db' }}>
+        <h1 className="code f2-1">Amplify Notetaker</h1>
+        <form>
+          <input type="text" />
+          <button>Add Note</button>
+        </form>
+
+        <div>
+          {this.renderNotes(this.state.notes)}
+        </div>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
